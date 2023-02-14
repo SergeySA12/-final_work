ba7cd280be37e17d9e651382435b66800c96fc04
strenadkosa <strenadkosa@mail.ru>
2023-02-14 22:33:32 +0300

    Созданили фон редактора и цвет текста.

--- notebook.py
+++ notebook.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
-from tkinter.filedialog import askopenfile, askopenfile
+from tkinter.filedialog import askopenfile, asksaveasfile
 
 file_name = None
 
@@ -8,8 +8,9 @@ def new_file():
     global file_name
     file_name = "Без названия"
     text.delete('1.0', END)
-def  save_as():
-    out = askopenfile(mode="w", defaultextension='.txt')
+
+def save_as():
+    out = asksaveasfile(mode='w', defaultextension='.txt')
     data = text.get('1.0', END)
     try:
         out.write(data.rstrip())
@@ -17,13 +18,14 @@ def  save_as():
         messagebox.showerror("Упс!, нельзя сохранить файл")
 
 def open_file():
-    global  file_name
+    global file_name
     inp = askopenfile(mode='r')
     if inp is None:
-        return file_name = inp.name
+        return
+        file_name = inp.name
     data = inp.read()
-    text.delete('1,0', END)
-    text.insert('1,0', data)
+    text.delete('1.0', END)
+    text.insert('1.0', data)
 
 root = Tk()
 root.title("Заметки")
@@ -31,9 +33,16 @@ root.geometry("400x400")
 
 menu_bar = Menu(root)
 file_menu = Menu(menu_bar)
+
+file_menu.add_command(label="Новый", command=new_file)
+file_menu.add_command(label="Открыть", command=open_file)
+file_menu.add_command(label="Сохранить как", command=save_as)
+
+# Создание кнопки.
 menu_bar.add_cascade(label="Файл", menu=file_menu)
 
-text = Text(root, width=400, height=400)
+# Создание размеров техтового файла.
+text = Text(root, width=400, height=400, background="Grey", fg='Lime')
 text.pack()
 
 root.config(menu=menu_bar)
