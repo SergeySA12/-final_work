ca761fd0b4aded83a0d03fb308496d32a456ea99
strenadkosa <strenadkosa@mail.ru>
2023-02-14 20:10:50 +0300

    Созданили функцию сохранения файла и открытия.

--- notebook.py
+++ notebook.py
@@ -8,6 +8,22 @@ def new_file():
     global file_name
     file_name = "Без названия"
     text.delete('1.0', END)
+def  save_as():
+    out = askopenfile(mode="w", defaultextension='.txt')
+    data = text.get('1.0', END)
+    try:
+        out.write(data.rstrip())
+    except Exception:
+        messagebox.showerror("Упс!, нельзя сохранить файл")
+
+def open_file():
+    global  file_name
+    inp = askopenfile(mode='r')
+    if inp is None:
+        return file_name = inp.name
+    data = inp.read()
+    text.delete('1,0', END)
+    text.insert('1,0', data)
 
 root = Tk()
 root.title("Заметки")
