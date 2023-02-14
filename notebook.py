c33338403648024ae0b0a6429671b22fceba4808
strenadkosa <strenadkosa@mail.ru>
2023-02-14 20:10:50 +0300

    Создание окна заметок.

--- notebook.py
+++ notebook.py
@@ -1,1 +1,10 @@
-print("Hello")
+from tkinter import *
+from tkinter import messagebox
+from tkinter.filedialog import askopenfile, askopenfile
+
+root = Tk()
+root.title("Заметки")
+root.geometry("400x400")
+
+
+root.mainloop()
